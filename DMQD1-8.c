d32d33fec45c6f4e8bb171f9c1789db5c4030668
Wanan2302 <144974521+Wanan2302@users.noreply.github.com>
2023-09-16 11:28:19 +0800

    Add files via upload

---
+++ DMQD1-8.c
@@ -0,0 +1,14 @@
+//为两个变量赋整数值并显示
+#include <stdio.h>
+int main ()
+{
+    int x,y;                   
+    
+    x=57;
+    y=x+10;                   //在头起定义方便更改
+    
+    printf ("x=%d\n",x);      //感觉有很多种写法，详见P12（可以写里面）
+    printf ("y=%d\n",y);
+    
+    return 0;
+}
