0563f6b9f4349b49d49b6c7a4b83194dea1ad737
Wanan2302 <144974521+Wanan2302@users.noreply.github.com>
2023-09-27 23:20:45 +0800

    Add files via upload

---
+++ DMQD4-11.c
@@ -0,0 +1,17 @@
+//递增显示从0到输入的正整数为止的各个整数（使用for语句）
+#include <stdio.h>
+
+int main (void)
+{
+    int no=0;
+    int i=0;
+
+    printf ("请输入一个正整数：");
+    scanf ("%d",&no);
+
+    for (i=0;i<=no;i++)
+        printf ("%d ",i);
+    putchar ('\n');
+
+    return 0;
+}
